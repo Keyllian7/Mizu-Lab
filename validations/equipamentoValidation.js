85d83b057766aa6fe3b91d1ba620dfba5d4cbad5
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-22 13:36:59 -0300

    Melhoria nas validações

--- validations/equipamentoValidation.js
+++ validations/equipamentoValidation.js
@@ -1,25 +1,32 @@
 const Joi = require('joi');
+const ToleranciaEnum = require('../enums/Tolerancia');
+const ProcedimentoEnum = require('../enums/Procedimento');
+const RegistroEnum = require('../enums/Registro');
+const MetodoEnum = require('../enums/Metodo');
+const EnsaioEnum = require('../enums/Ensaio');
+const PeriocidadeEnum = require('../enums/Periocidade');
+const TesteEnum = require('../enums/Teste');
 
 const schemaEquipamento = Joi.object({
     responsavel: Joi.string().required(),
     nome: Joi.string().required(),
     tag: Joi.string().required(),
     norma: Joi.string().required(),
-    ensaio: Joi.string().required(),
-    metodo: Joi.string().required(),
-    teste: Joi.string().required(),
+    ensaio: Joi.string().required().valid(...Object.values(EnsaioEnum)),
+    metodo: Joi.string().required().valid(...Object.values(MetodoEnum)),
+    teste: Joi.string().required().valid(...Object.values(TesteEnum)),
     fabricante: Joi.string().required(),
     modelo: Joi.string().required(),
     serie: Joi.string().required(),
     capacidade: Joi.string().required(),
     ponto_calibracao: Joi.string().required(),
-    tolerancia: Joi.string().required(),
-    procedimento: Joi.string().required(),
-    registro: Joi.string().required(),
+    tolerancia: Joi.string().required().valid(...Object.values(ToleranciaEnum)),
+    procedimento: Joi.string().required().valid(...Object.values(ProcedimentoEnum)),
+    registro: Joi.string().required().valid(...Object.values(RegistroEnum)),
 })
 
 const calibrbarEquipamento = Joi.object({
-    periocidade: Joi.string().required(),
+    periocidade: Joi.string().required().valid(...Object.values(PeriocidadeEnum)),
     observacoes: Joi.string().required(),
 })
 
